3b33cfe0edb7f3075cdf497dabfe830bcf8eb3b8
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-06-15 22:36:28 +0300

    Change #1422

---
+++ assets/js/rada.js
@@ -0,0 +1,7 @@
+function detailFormatter(index, row) {
+  var html = []
+  $.each(row, function (key, value) {
+    html.push('<p><b>' + key + ':</b> ' + value + '</p>')
+  })
+  return html.join('')
+}
