5aaf648af4187ad13266250422d747fd835b70d0
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-06-16 20:04:51 +0300

    Change #1451

--- assets/js/rada.js
+++ assets/js/rada.js
@@ -1,16 +1,15 @@
 function detailFormatter(index, row) {
-  var row = ({
-      "#": row.id,
-      "Квартира": row.apartment,
-      "Кімнат": row.rooms,
-      "Площа": row.area,
-      "Ціна": row.price,
-      "Продавець": row.seller,
-      "Телефон": row.phone
-  })
   var html = []
+  const title = {
+    'id': 'ID',
+    'apartment': 'Квартира',
+    'rooms': 'Кімнат',
+    'price': 'Ціна',
+    'seller': 'Продавець',
+    'phone': 'Телефон'
+  }
   $.each(row, function (key, value) {
-    html.push('<span class="col"><b>' + key + ':</b> ' + value + '</span>')
+    html.push('<span class="col"><b>' + title[key] + ':</b> ' + value + '</span>')
   })
   return html.join('')
 }
