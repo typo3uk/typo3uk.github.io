0adbefcd95779db48812f497482df68cb5fb169e
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-06-16 23:24:10 +0300

    Change #1464

--- assets/js/rada.js
+++ assets/js/rada.js
@@ -1,7 +1,7 @@
 
 function propertyFormatter(value, row, index) {
   // return [row.property, row.rooms, row.area].join(', ')
-  return 'Продається ' + row.property + ', Кімнат: ' + row.rooms + ', Площа: ' + row.area;
+  return 'Продається ' + row.property + ', Кімнат: ' + row.rooms + ', Площа: ' + row.area + '<a href="javascript:void(0)">...</a>';
 }
 
 function detailFormatter(index, row) {
