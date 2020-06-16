d41eeffa2b885a0ddddf4f93cc269615b3ba6ae1
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-06-16 18:05:06 +0300

    Change #1446

--- assets/js/rada.js
+++ assets/js/rada.js
@@ -1,40 +1,11 @@
-// $(function() {
-//   $table.bootstrapTable({
-//     columns: [
-//       {
-//         field: 'id',
-//         title: 'Номер'
-//       },
-//       {
-//         field: 'apartmente',
-//         title: 'Квартира'
-//       },
-//       {
-//         field: 'rooms',
-//         title: 'Кімнат'
-//       },
-//       {
-//         field: 'area',
-//         title: 'Площа'
-//       },
-//       {
-//         field: 'price',
-//         title: 'Ціна'
-//       }
-//     ]
-//   })
-// };
-
 function detailFormatter(index, row) {
-
   var row = ({
-      Title: row.id,
-      Apartmente: row.apartmente,
-      Rooms: row.rooms,
-      Area: row.area,
-      Price: row.price
+      №: row.id,
+      Квартира: row.apartmente,
+      Кімнат: row.rooms,
+      Площа: row.area,
+      Ціна: row.price
   })
-
   var html = []
   $.each(row, function (key, value) {
     html.push('<span class="col"><b>' + key + ':</b> ' + value + '</span>')
