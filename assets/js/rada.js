c302d9ce1c0ef716488de0ed7084f41d7ff2a6d3
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-06-17 21:43:16 +0300

    Change #1519

--- assets/js/rada.js
+++ assets/js/rada.js
@@ -8,10 +8,16 @@
 //     search: true
 //   })
 // })
+var $table = $('')
+
+$(function() {
+  $table.bootstrapTable()
+})
 
 
-$('#phones').bootstrapTable({
-  function detailFormatter(index, row) {
+function detailFormatter(index, row) {
+  if ($table == '#phones') {
+
     var html = []
     const title = {
       'id': 'ID',
@@ -26,36 +32,35 @@ $('#phones').bootstrapTable({
       html.push('<span class="col-12 col-sm-2 col-md-4"><b>' + title[key] + '</b>: ' + value + '</span>')
     })
     return '<span class="row mx-0">' + html.join('') + '</span>'
+
   }
-});
+}
 
-$('#table').bootstrapTable({
 
-  function propertyFormatter(value, row, index) {
-    return 'Продається <span class="text-lowercase"><strong>' + row.property + '</strong></span>, кімнат <strong>' + row.rooms + '</strong>, площа <strong>' + row.area + '</strong>, <a href="javascript:void(0)">ще...</a>';
+
+function propertyFormatter(value, row, index) {
+  return 'Продається <span class="text-lowercase"><strong>' + row.property + '</strong></span>, кімнат <strong>' + row.rooms + '</strong>, площа <strong>' + row.area + '</strong>, <a href="javascript:void(0)">ще...</a>';
+}
+
+function detailFormatter(index, row) {
+  var html = []
+  const title = {
+    'id': 'ID',
+    'property': 'Власність',
+    'rooms': 'Кімнат',
+    "area": "Площа",
+    'price': 'Ціна',
+    'seller': 'Продавець',
+    'phone': 'Телефон'
   }
+  $.each(row, function (key, value) {
+    html.push('<span class="col-12 col-sm-2 col-md-4"><b>' + title[key] + '</b>: ' + value + '</span>')
+  })
+  return '<span class="row mx-0">' + html.join('') + '</span>'
+}
 
-  function detailFormatter(index, row) {
-    var html = []
-    const title = {
-      'id': 'ID',
-      'property': 'Власність',
-      'rooms': 'Кімнат',
-      "area": "Площа",
-      'price': 'Ціна',
-      'seller': 'Продавець',
-      'phone': 'Телефон'
-    }
-    $.each(row, function (key, value) {
-      html.push('<span class="col-12 col-sm-2 col-md-4"><b>' + title[key] + '</b>: ' + value + '</span>')
-    })
-    return '<span class="row mx-0">' + html.join('') + '</span>'
-  }
-
-  function priceSorter(a, b) {
-    var aa = a.replace('$', '')
-    var bb = b.replace('$', '')
-    return aa - bb
-  }
-
-});
+function priceSorter(a, b) {
+  var aa = a.replace('$', '')
+  var bb = b.replace('$', '')
+  return aa - bb
+}
