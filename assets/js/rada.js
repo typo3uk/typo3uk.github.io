db35c6f07f0098c3356ef38bfc29ee39e4fbbca7
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-06-17 19:17:53 +0300

    Change #1512

--- assets/js/rada.js
+++ assets/js/rada.js
@@ -1,27 +1,30 @@
+$('#table').bootstrapTable({
 
-function propertyFormatter(value, row, index) {
-  return 'Продається <span class="text-lowercase"><strong>' + row.property + '</strong></span>, кімнат <strong>' + row.rooms + '</strong>, площа <strong>' + row.area + '</strong>, <a href="javascript:void(0)">ще...</a>';
-}
-
-function detailFormatter(index, row) {
-  var html = []
-  const title = {
-    'id': 'ID',
-    'property': 'Власність',
-    'rooms': 'Кімнат',
-    "area": "Площа",
-    'price': 'Ціна',
-    'seller': 'Продавець',
-    'phone': 'Телефон'
+  function propertyFormatter(value, row, index) {
+    return 'Продається <span class="text-lowercase"><strong>' + row.property + '</strong></span>, кімнат <strong>' + row.rooms + '</strong>, площа <strong>' + row.area + '</strong>, <a href="javascript:void(0)">ще...</a>';
   }
-  $.each(row, function (key, value) {
-    html.push('<span class="col-12 col-sm-2 col-md-4"><b>' + title[key] + '</b>: ' + value + '</span>')
-  })
-  return '<span class="row mx-0">' + html.join('') + '</span>'
-}
 
-function priceSorter(a, b) {
-  var aa = a.replace('$', '')
-  var bb = b.replace('$', '')
-  return aa - bb
-}
+  function detailFormatter(index, row) {
+    var html = []
+    const title = {
+      'id': 'ID',
+      'property': 'Власність',
+      'rooms': 'Кімнат',
+      "area": "Площа",
+      'price': 'Ціна',
+      'seller': 'Продавець',
+      'phone': 'Телефон'
+    }
+    $.each(row, function (key, value) {
+      html.push('<span class="col-12 col-sm-2 col-md-4"><b>' + title[key] + '</b>: ' + value + '</span>')
+    })
+    return '<span class="row mx-0">' + html.join('') + '</span>'
+  }
+
+  function priceSorter(a, b) {
+    var aa = a.replace('$', '')
+    var bb = b.replace('$', '')
+    return aa - bb
+  }
+
+});
