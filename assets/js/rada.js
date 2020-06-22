a870a95e23e257c84766ac039b813aa162bd18be
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-06-22 20:30:06 +0300

    Change #1632

--- assets/js/rada.js
+++ assets/js/rada.js
@@ -1,32 +1,4 @@
 
-// var $table = $('#table')
-// var $table = document.getElementsByTagName('table')[0].id
-// var $table = document.getElementById("table").id;
-// var $table = table.id
-
-// $("table").delegate("tr", "click", function() {
-//    var $table = $(this).attr('id');
-// });
-var $table = $('#phones')
-var selectedRow = {}
-
-$(function() {
-  $table.on('click-row.bs.table', function (e, row, $element) {
-    selectedRow = row
-    $('.active').removeClass('active')
-    $($element).addClass('active')
-  })
-})
-
-function rowStyle(row) {
-  if (row.id === selectedRow.id) {
-    return {
-      classes: 'active'
-    }
-  }
-  return {}
-}
-
 function fullnameFormatter(value, row, index) {
   return '<strong>' + row.fullname + '</strong> - ' + row.position + '';
 }
